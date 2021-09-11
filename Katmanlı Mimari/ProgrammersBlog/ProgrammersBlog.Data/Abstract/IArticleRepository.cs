6c8b2f171e511fddb5577fdf46f616a1ce5614c0
bilgekara <karabilgenur@gmail.com>
2021-09-11 14:27:35 +0300

    Data Layer entities

---
+++ Katmanlı Mimari/ProgrammersBlog/ProgrammersBlog.Data/Abstract/IArticleRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IArticleRepository:IEntityRepository<Article>
+    {
+        // kategoriye göre article gelsin
+        // IList<Article> GetArticleByCategory(int categoryId);
+
+    }
+}
---
+++ Katmanlı Mimari/ProgrammersBlog/ProgrammersBlog.Data/Concrete/EntityFramework/Repositories/EfArticleRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Repositories
+{
+    // entity framework yapısı üzerine kurulmuş bir repository,
+    // daha buyuk projelerde enittiy framework kullanılmayabilir docker,ado.net vb kullanılabilir
+
+    public class EfArticleRepository : EfEntityRepositoryBase<Article>, IArticleRepository
+    {
+        // constructor icinde bir db context vermeliyiz
+        public EfArticleRepository(DbContext context) : base(context)
+        {
+        }
+
+        //public IList<Article> GetArticleByCategory(int categoryId)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
