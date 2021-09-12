c55dd25ebb6ebec62f6f0a86211df309efb77bd4
bilgekara <karabilgenur@gmail.com>
2021-09-12 15:57:01 +0300

    utilities

---
+++ Katmanlı Mimari/ProgrammersBlog/ProgrammersBlog.Shared/Utilities/Results/Abstract/IDataResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
+{
+    // out: bir kategoriyi tek de taşımak isteyebilirim veya Ilist
+    // ya da IEnumarable olarak da taşımak isteyebilirim.
+    // her iki işlem için de farklı farklı proportieler tanımlamak yerine
+    // bu şekilde tek bir prportie içerisinde ister bir liste istersek de te bir entitie taşıyabiliyor olacaz.
+    public interface IDataResult<out T> : IResult
+    {
+        public T Data { get; } // new DataResult<Category>(ResultStatus.Success,category);
+                               // new DataResult<IList<Category>>(ResultStatus.Success, categoryList);
+    }
+}
---
+++ Katmanlı Mimari/ProgrammersBlog/ProgrammersBlog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,21 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        /* Kullanıcılara bir sonuç döneceksek bu result durumunu 
+         * kullanıcıyla paylaşmamız gerekiyor. Yani buradaki işleme 
+         * başarılı mı, hatalı mı, uyarı mı yoksa sadece bilgilendirme 
+         * işlemi mi? Bunu state durum olarak tutacaz.
+         */
+        public ResultStatus ResultStatus { get; } // ResultStatus.Success // ResultStatus.Error
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
