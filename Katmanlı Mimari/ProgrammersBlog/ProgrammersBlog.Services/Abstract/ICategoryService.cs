cc95bfa1e86a1f982c39d8a538cf4e785df84f1e
bilgekara <karabilgenur@gmail.com>
2021-09-13 09:28:00 +0300

    DTO

--- Katmanlı Mimari/ProgrammersBlog/ProgrammersBlog.Services/Abstract/ICategoryService.cs
+++ Katmanlı Mimari/ProgrammersBlog/ProgrammersBlog.Services/Abstract/ICategoryService.cs
@@ -13,13 +13,14 @@ namespace ProgrammersBlog.Services.Abstract
     {
         Task<IDataResult<Category>> Get(int categoryId);
         Task<IDataResult<IList<Category>>> GetAll();
+        Task<IDataResult<IList<Category>>> GetAllByNonDeleted();
         // bir veri ekleyince IResult dönücez ve veri eklerken de kategorinin
         // tamamını değil sadece front end tarafını kullanıcıdan isteyecez
         Task<IResult> Add(CategoryAddDto categoryAddDto,string createdByName);
         Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
         // bu gercekten category'i silmicek delete degerini true yapacak
         //getAll diye cagirdigimizda delete olanlar gelmiyor olacak
-        Task<IResult> Delete(int categoryId);
+        Task<IResult> Delete(int categoryId, string modifiedByName);
         // veri tabanından silincek
         Task<IResult> HardDelete(int categoryId);
     }
