93ce20b24458db5a028eaa5ddbe33b05785c92cf
bilgekara <karabilgenur@gmail.com>
2021-09-16 00:29:34 +0300

    admin category

---
+++ Katmanlı Mimari/ProgrammersBlog/ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            /* actegory service içinde bir data result donuyoruz, 
+             * bu data result içerisindeki result status yani bu işlemin 
+             * durumu success ise başarılıysa ona göre işlem yapıcaz
+             * var result ile data result almış olduk
+             */
+            var result = await _categoryService.GetAll();
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+
+            return View();
+        }
+    }
+}
